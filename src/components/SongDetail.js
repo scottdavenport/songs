6e44bd28f896a7f0cd2efea5e863a5774389d0c7
scottdavenport <scott@pinpoint.com>
2021-01-16 18:34:33 -0500

    Conditional Rendering

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SongDetail = (props) => {
-	console.log(props);
-	return <div>SongDetail</div>;
+const SongDetail = ({ song }) => {
+	if (!song) {
+		return <div>Select a Song</div>;
+	}
+
+	return (
+		<div>
+			<h3> Details for:</h3>
+			<p>
+				Title: {song.title}
+				<br />
+				Duration: {song.duration}
+			</p>
+		</div>
+	);
 };
 
 const mapStateToProps = (state) => {
-	return { song: state.selectedSong };
+	return {
+		song: state.selectedSong,
+		duration: state.duration,
+	};
 };
 
 export default connect(mapStateToProps)(SongDetail);
