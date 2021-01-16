578390b11cc8e46723780f09977827b1c3e1efe4
scottdavenport <scott@pinpoint.com>
2021-01-15 22:32:17 -0500

    The Connect Function

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'react-redux';
+import { createStore } from 'redux';
 // my own code
 import App from './components/App';
 import reducers from './reducers';
